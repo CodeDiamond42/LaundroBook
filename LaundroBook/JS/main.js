8d68d84cac7746aa232f59e557122a6788b727e5
jazminReddy <jazminreddy21@gmail.com>
2026-07-17 15:50:20 +0200

    Add files via upload
    initial upload of Luandrobook Website

---
+++ LaundroBook/JS/main.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+
+    alert("Welcome to LaundroBook!");
+
+};
+
+// Book button interaction
+document.getElementById("bookBtn").addEventListener("click", function () {
+
+    alert("Booking page coming soon!");
+
+});
